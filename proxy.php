ce968c5838e1a7ee8b24ec3899d2bc2cc4f08807
Alexandre Cabrera <28904560+alexandrexose@users.noreply.github.com>
2024-08-27 06:47:36 +0200

    proxy.php server to resolve CORS

---
+++ proxy.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$url = "http://bored.api.lewagon.com" . $_SERVER['REQUEST_URI'];
+
+// Initialize cURL session
+$ch = curl_init($url);
+
+// Set options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+// Execute cURL session
+$response = curl_exec($ch);
+
+// Close cURL session
+curl_close($ch);
+
+// Output the response
+echo $response;
+?>
